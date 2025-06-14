36acfe42e2fdc0eb26ffc605ca5e1e69f3e03058
Javier <angel.gonzalez.preciado@uabc.edu.mx>
2025-06-14 04:30:16 -0700

    feat: Se agrego pantalla del progreso del usuario

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+/*
 import 'package:flutter/material.dart';
 import 'screens/login.dart';
 import 'strings/strings_utils.dart';
@@ -19,3 +20,21 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+*/
+
+
+import 'package:flutter/material.dart';
+import 'screens/progreso.dart';
+import '../strings/strings_utils.dart';
+
+void main() => runApp(MyApp());
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: AppStrings.tituloAppBar,
+      home: ProgresoModulosScreen(),
+    );
+  }
+}
+
